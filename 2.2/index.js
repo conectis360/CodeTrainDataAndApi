33ead42f35bc46f7d70a123bd284a4e66d880fea
conectis360 <conectis360@gmail.com>
2020-03-30 22:12:55 -0300

    2.3

--- 2.2/index.js
+++ 2.2/index.js
@@ -1,19 +1,25 @@
 const express = require('express');
+const Datastore = require('nedb');
 const app = express();
 
 app.listen(3000, () => console.log('Escutando a 3000'));
 app.use(express.json({limit: '1mb'}));
 app.use(express.static('public'));
 
-const database = [];
+const database = new Datastore('database.db');
+database.loadDatabase();
 
 app.post('/api' , (request, response) => {
     console.log(request.body);
     const json = request.body;
-    database.push(json);
+    const timestamp = Date.now();
+    Date.timestamp = timestamp;
+
+    database.insert(json);
     console.log(database);
     response.json({
         status: "success",
+        timestamp: timestamp,
         latitute: json.lat,
         longitute: json.long 
     });
